de2c8fe9e327638173ed6443ad01a2f0a4aa5dd1
Michael <it@michaelsunier.ch>
2024-06-25 15:58:13 +0200

    Test

---
+++ site-modules/profile/manifests/apache.pp
@@ -0,0 +1,38 @@
+class profile::apache (
+  Array[String[1]] $mods = [],
+  Boolean $default_vhost = false,
+  Boolean $purge_configs = false,
+  Hash $vhosts = {},
+) {
+
+  $vhosts.each |$k,$m| {
+    apache::vhost { $k:
+      * => $m
+    }
+  }
+
+  class { 'apache':
+    mpm_module => false,
+    default_vhost => $default_vhost,
+    purge_configs => $purge_configs,
+  }
+
+  # Most basic vhost
+  apache::vhost { 'example1.com':
+    port    => 80,
+    docroot => '/var/www1',
+    servername => 'example1.com',
+    error_log => false,
+    access_log => false,
+    directories => [ {} ],
+  }
+  # Most basic vhost
+  apache::vhost { 'example2.com':
+    port    => 81,
+    docroot => '/var/www2',
+    servername => 'example2.com',
+    error_log => false,
+    access_log => false,
+    directories => [ {} ],
+  }
+}
--- site-modules/profile/manifests/base.pp
+++ site-modules/profile/manifests/base.pp
@@ -1,7 +1,7 @@
 # The base profile should include component modules that will be on all nodes
 class profile::apache {
   class { 'apache':
-    mpm_module    => true,
+    mpm_module    => false,
     default_mods  => false,
     default_vhost => false,
     purge_configs => false,
